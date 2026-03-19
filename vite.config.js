f9979cb8e8e60627aa65d39b95aebeb5ca5d64e7
Deluxe08 <ariagacha@gmail.com>
2026-03-19 16:24:51 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  // CRITICAL: This must match your GitHub repo name exactly
   base: '/TEJ4-Capstone/',
   build: {
     rollupOptions: {
