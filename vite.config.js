5c3997e9ad2a6dbcb74e44203098af47380f7964
Deluxe08 <ariagacha@gmail.com>
2026-03-20 07:42:24 -0400

    Update base path configuration for Vite

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react(), tailwindcss()],
-    base: "/TEJ4-Capstone/",
+    base: process.env.VITE_BASE_PATH || "/react-vite-deploy" 
     build: {
         rollupOptions: {
             output: {
